023bebeede27c9b2a8eaee82b2fb5f043f45ea08
nabeel-ahsan <itsme007nabeel@gmail.com>
2024-02-12 21:08:30 +0530

    Updated user.js

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,6 +1,8 @@
 const route = require("express").Router();
 
 routes.post('/', (req,res) => {
+    const username = req.body.username;
+    const password = req.body.password;
 })
 
 module.exports = route
