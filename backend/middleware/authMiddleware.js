83f661fee1ce6d1fa6e0b293b348c667f6cf3a69
Mohd Saquib Mansoori <msmsaq114@gmail.com>
2024-02-13 22:39:41 +0530

    Solve mini bug in autMiddleware and implement logic in signin

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,17 +1,19 @@
 const jwt = require("jsonwebtoken")
 
+const JWTSECRET = "1234abcd"
 
 function authMiddleware (req,res,next){
     const authHeader = req.headers.authorization
     if (!authHeader || !authHeader.startsWith('Bearer ')) {
         return res.status(403).json({});
     }
-
     const token = authHeader.split(' ')[1];
     try {
+       
         const decoded = jwt.verify(token, JWTSECRET); 
-        req.user = decoded; 
-        console.log(decoded);
+        req.userId = decoded.UserId; 
+       
+       
         next(); 
     } catch (error) {
         return res.status(401).json({ message: 'Invalid token' });
