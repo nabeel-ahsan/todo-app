f7783d7c2ebd8fc53f2f20e515109d8fe9d7e528
nabeel-ahsan <itsme007nabeel@gmail.com>
2024-02-13 21:31:20 +0530

    Added jwt authentication logic

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -7,6 +7,16 @@ function authMiddleware (req,res,next){
         return res.status(403).json({});
     }
 
+    const token = authHeader.split(' ')[1];
+    try {
+        const decoded = jwt.verify(token, JWTSECRET); 
+        req.user = decoded; 
+        console.log(decoded);
+        next(); 
+    } catch (error) {
+        return res.status(401).json({ message: 'Invalid token' });
+    }
+
     next()
 }
 
