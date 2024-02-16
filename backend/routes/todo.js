1e0e0245ace684a39335beae98aa06dfc26321c8
nabeel-ahsan <itsme007nabeel@gmail.com>
2024-02-16 22:16:08 +0530

    updated todo.js

--- backend/routes/todo.js
+++ backend/routes/todo.js
@@ -1,3 +1,15 @@
 const route = require("express").Router();
 
+const authMiddleware  = require("../middleware/authMiddleware");
+const {User,Todo} = require("../model/model.js");
+
+route.post('/todo', authMiddleware, async (req, res) => {
+    const userId = req.userId;
+    const {title,description} = req.body;
+    const todo = await Todo.create({title: title, description: description, completed:false, userId: userId})
+    res.json({
+        todo
+    })
+})
+
 module.exports = route
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,38 +1,37 @@
-
 const route = require("express").Router();
-const {User} = require("../model/model");
+const { User } = require("../model/model");
 const jwt = require("jsonwebtoken");
-const authMiddleware  = require("../middleware/authMiddleware")
+const authMiddleware = require("../middleware/authMiddleware");
 
-const JWTSECRET = "1234abcd"
-route.post('/signup', async (req,res) => {
-  try{
-
+const JWTSECRET = "1234abcd";
+route.post("/signup", async (req, res) => {
+  try {
     const username = req.body.username;
     const password = req.body.password;
-    
-    const user =  await User.create({username, password})
-    const token = jwt.sign({userId:user._id},JWTSECRET )
+
+    const user = await User.create({ username, password });
+    const token = jwt.sign({ userId: user._id }, JWTSECRET);
     res.status(200).json({
-      token : token
-    })
-  }catch(e){
+      token: token,
+    });
+  } catch (e) {
     res.status(400).json({
-      error:e.message
-    })
+      error: e.message,
+    });
   }
-})
+});
 
-route.post("/signin", authMiddleware, function (req, res) {
- try{ const token = jwt.sign({userId:req.userId},JWTSECRET )
-  res.status(200).json({
-    token : token
-  })}catch(e){
+route.post("/signin", function (req, res) {
+  try {
+    const token = jwt.sign({ userId: req.userId }, JWTSECRET);
+    res.status(200).json({
+      token: token,
+    });
+  } catch (e) {
     res.status(400).json({
-      error:e.message
-    })
+      error: e.message,
+    });
   }
- 
-  });
+});
 
-module.exports = route
+module.exports = route;
